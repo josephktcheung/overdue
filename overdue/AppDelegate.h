95dc31ba8800df4694b3dfb22663d90fe753fc1d
josephktcheung <josephktcheung@gmail.com>
2014-07-25 22:48:09 +0800

    Initial Commit

---
+++ overdue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  overdue
+//
+//  Created by Joseph Cheung on 25/7/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ overdue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  overdue
+//
+//  Created by Joseph Cheung on 25/7/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
